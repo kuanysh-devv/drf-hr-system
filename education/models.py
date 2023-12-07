d2f66ff23894192d7e1b68b21ad6997286a6370d
Kuanysh <kuanysh01012@gmail.com>
2023-12-07 17:24:49 +0600

    Transfer decree

--- education/models.py
+++ education/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from person.models import Person
@@ -44,6 +45,21 @@ class Attestation(models.Model):
     nextAttDateMin = models.DateField(null=True, blank=True, verbose_name=_("Next Attestation Date (Min)"))
     nextAttDateMax = models.DateField(null=True, blank=True, verbose_name=_("Next Attestation Date (Max)"))
     personId = models.ForeignKey(Person, on_delete=models.CASCADE, default=1, verbose_name=_("Person"))
+
+    def save(self, *args, **kwargs):
+        if not self.pk:  # Check if it's a new instance (not yet saved to the database)
+            if self.attResult == 'Соответствует':
+                # If attResult is 'Proshel', set nextAttDateMin to lastAttDate + 3 years
+                self.nextAttDateMin = self.lastAttDate + timezone.timedelta(days=3 * 365)
+                # Set nextAttDateMax to lastAttDate + 3 years 3 months
+                self.nextAttDateMax = self.lastAttDate + timezone.timedelta(days=(3 * 365) + (6 * 30))
+            elif self.attResult == 'Не соответствует':
+                # If attResult is 'Ne proshel', set nextAttDateMin to lastAttDate + 3 months
+                self.nextAttDateMin = self.lastAttDate + timezone.timedelta(days=3 * 30)
+                # Set nextAttDateMax to lastAttDate + 6 months
+                self.nextAttDateMax = self.lastAttDate + timezone.timedelta(days=6 * 30)
+
+        super(Attestation, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.personId) + ' ' + self.attResult
--- education/signals.py
+++ education/signals.py
@@ -1,40 +1,19 @@
-from datetime import timedelta
-
-from django.db.models.signals import post_save, post_init
+from django.db.models.signals import post_save, post_init, pre_init, pre_save
 from django.dispatch import receiver
-
+from django.utils import timezone
 from education.models import Attestation
-from military_rank.models import RankInfo
 
 
-@receiver(post_save, sender=Attestation)
-def attestation_post_save(sender, instance, created, **kwargs):
-    if created:
-        if instance.attResult == 'Соответствует':
-            # If attResult is 'Proshel', set nextAttDateMin to lastAttDate + 3 years
-            instance.nextAttDateMin = instance.lastAttDate + timedelta(days=3 * 365)
-            # Set nextAttDateMax to lastAttDate + 3 years 3 months
-            instance.nextAttDateMax = instance.lastAttDate + timedelta(days=(3 * 365) + (6 * 30))
-        elif instance.attResult == 'Не соответствует':
-            # If attResult is 'Ne proshel', set nextAttDateMin to lastAttDate + 3 months
-            instance.nextAttDateMin = instance.lastAttDate + timedelta(days=3 * 30)
-            # Set nextAttDateMax to lastAttDate + 6 months
-            instance.nextAttDateMax = instance.lastAttDate + timedelta(days=6 * 30)
-
-        # Save the updated instance
-        instance.save()
-
-
-@receiver(post_init, sender=Attestation)
-def attestation_post_init(sender, instance, **kwargs):
+@receiver(pre_save, sender=Attestation)
+def attestation_pre_save(sender, instance, **kwargs):
     if instance.attResult == 'Соответствует':
         # If attResult is 'Proshel', set nextAttDateMin to lastAttDate + 3 years
-        instance.nextAttDateMin = instance.lastAttDate + timedelta(days=3 * 365)
+        instance.nextAttDateMin = instance.lastAttDate + timezone.timedelta(days=3 * 365)
         # Set nextAttDateMax to lastAttDate + 3 years 3 months
-        instance.nextAttDateMax = instance.lastAttDate + timedelta(days=(3 * 365) + (6 * 30))
+        instance.nextAttDateMax = instance.lastAttDate + timezone.timedelta(days=(3 * 365) + (6 * 30))
     elif instance.attResult == 'Не соответствует':
-        # If attResult is 'Ne proshel', set nextAttDateMin to lastAttDate + 3 months
-        instance.nextAttDateMin = instance.lastAttDate + timedelta(days=3 * 30)
+         # If attResult is 'Ne proshel', set nextAttDateMin to lastAttDate + 3 months
+        instance.nextAttDateMin = instance.lastAttDate + timezone.timedelta(days=3 * 30)
         # Set nextAttDateMax to lastAttDate + 6 months
-        instance.nextAttDateMax = instance.lastAttDate + timedelta(days=6 * 30)
+        instance.nextAttDateMax = instance.lastAttDate + timezone.timedelta(days=6 * 30)
 
