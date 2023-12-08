09c97413e8bfecfea5875d0d48b61d977a52a40f
Kuanysh <kuanysh01012@gmail.com>
2023-12-08 11:03:37 +0600

    Urls.py update

--- education/signals.py
+++ education/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, post_init, pre_init, pre_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.utils import timezone
 from education.models import Attestation
